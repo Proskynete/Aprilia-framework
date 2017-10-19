989c3cc6ce32f2655a91a9e55e08ef5e0aa0ca2d
Eduardo Alvarez <eduardo.alvarez@globant.com>
2017-10-19 12:42:28 -0300

    Compress css file and add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 const gulp        = require('gulp');
 const sass        = require('gulp-sass');
+const sourcemaps  = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
 const reload      = browserSync.reload;
 
