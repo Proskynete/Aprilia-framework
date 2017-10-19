d7fb515a375df1bbb6aa6cb1bf125a4a995cdc1e
Eduardo Alvarez <eduardo.alvarez@globant.com>
2017-10-19 13:03:48 -0300

    Add 'autoprefixer'

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 'use stric';
 
-const gulp        = require('gulp');
-const sass        = require('gulp-sass');
-const sourcemaps  = require('gulp-sourcemaps');
-const browserSync = require('browser-sync').create();
-const reload      = browserSync.reload;
+const gulp          = require('gulp');
+const sass          = require('gulp-sass');
+const sourcemaps    = require('gulp-sourcemaps');
+const autoprefixer  = require('gulp-autoprefixer');
+const browserSync   = require('browser-sync').create();
+const reload        = browserSync.reload;
 
 // Static server
 gulp.task('server', () =>{
@@ -18,13 +19,14 @@ gulp.task('server', () =>{
 // Watch html and scss files
 gulp.task('watch', () => {
     gulp.watch("./assets/scss/*.scss", ['sass']);
-    gulp.watch("./index.html").on('change', browserSync.reload);
+    gulp.watch("./index.html").on('change', reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', () =>{
     return gulp.src("./assets/scss/*.scss")
         .pipe(sourcemaps.init())
+        .pipe(autoprefixer({browsers: ["last 2 version", "> 5% in US", "ie 8", "ie 7"], cascade: false}))
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest("./public/css"))
